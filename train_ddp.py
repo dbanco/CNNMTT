93b94063229f277d53216519f3de59b033fdbe76
dbanco <dpqb10@gmail.com>
2025-07-19 20:10:37 -0700

    update

--- train_ddp.py
+++ train_ddp.py
@@ -185,7 +185,7 @@ if __name__ == '__main__':
     parser.add_argument('--height', type=int, default=32)
     parser.add_argument('--width', type=int, default=96)
     parser.add_argument('--batch_size', type=int, default=1)
-    parser.add_argument('--num_epochs', type=int, default=60)
+    parser.add_argument('--num_epochs', type=int, default=5)
     parser.add_argument('--lr', type=float, default=1e-3)
     parser.add_argument('--num_outputs', type=int, default=1)
     args = parser.parse_args()
