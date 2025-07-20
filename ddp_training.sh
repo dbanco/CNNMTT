9370f1b1254c9e2b242a81de58597d07db023e6d
dbanco <dpqb10@gmail.com>
2025-07-19 19:16:55 -0700

    update

--- ddp_training.sh
+++ ddp_training.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #SBATCH --job-name=mtt_ddp
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=2           # Number of processes (GPUs) per node
+#SBATCH --ntasks-per-node=4           # Number of processes (GPUs) per node
 #SBATCH --gres=gpu:4                  # Adjust if you have more GPUs
 #SBATCH --cpus-per-task=4             # Number of CPU cores per task
-#SBATCH --mem=32G
+#SBATCH --mem=4g
 #SBATCH --time=04:00:00
 
 module load anaconda
