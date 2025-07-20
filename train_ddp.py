eab6fff2bcfd27797f581afa2978e93808544ed5
dbanco <dpqb10@gmail.com>
2025-07-20 10:00:25 -0700

    update

--- train_ddp.py
+++ train_ddp.py
@@ -182,7 +182,7 @@ def main(args):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--num_train_samples', type=int, default=100)
+    parser.add_argument('--num_train_samples', type=int, default=1000000)
     parser.add_argument('--sequence_length', type=int, default=30)
     parser.add_argument('--height', type=int, default=32)
     parser.add_argument('--width', type=int, default=96)
